379bbe068dfa3a2befdc04dd05019358a3017796
Khalid Shawki <khalid.i.shawki@gmail.com>
2023-10-13 01:01:36 +0300

    add

---
+++ app/components/main/Contactless.jsx
@@ -0,0 +1,59 @@
+'use client';
+import Heading from './Heading';
+import './contactless.css';
+
+const data = [
+  {
+    img: '/contactless-delivery/icon_handwash.png',
+    desc: 'Our delivery drivers sanitize their hands before and after every food order',
+  },
+  {
+    img: '/contactless-delivery/icon_delivery_mask.png',
+    desc: 'All our delivery drivers wear masks and gloves',
+  },
+  {
+    img: '/contactless-delivery/icon_delivery_bag.png',
+    desc: 'All delivery bags are sanitized before and after every order',
+  },
+  {
+    img: '/contactless-delivery/icon_delivery_bag.png',
+    desc: 'Takeaway food bags will be sealed with tape',
+  },
+];
+
+const Card = ({ img, desc }) => {
+  return (
+    <article className='card'>
+      <img src={img} width={56} alt={desc} />
+      <p className='' style={{ fontSize: '11px', lineHeight: '1.3' }}>
+        {desc}
+      </p>
+    </article>
+  );
+};
+
+export default function Contact() {
+  return (
+    <section className='container' style={{ margin: '28px 0' }}>
+      <Heading
+        title='Contactless Delivery'
+        img='/icons/contactless-delivery.png'
+        viewAll='false'
+      />
+
+      <div className='flex contactless'>
+        <article className='card special-card'>
+          <img
+            src='/contactless-delivery/banner.png'
+            
+          />
+        </article>
+        {data.map((item) => (
+          <div className='' key={item.img}>
+            <Card img={item.img} desc={item.desc} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ app/components/main/Heading.jsx
@@ -0,0 +1,21 @@
+'use client';
+import './main.css';
+
+const Heading = (props) => {
+  return (
+    <div className='flex  title-container items-center justify-between'>
+      <div className='title flex items-center jusitfy-between'>
+        <h3>{props.title}</h3>
+        <img src={props.img} alt='' />
+      </div>
+      {props.viewAll != 'false' && (
+        <div className='view-all'>
+          <h4>View All</h4>
+          <img src='/icons/left-arrow.svg' alt='view all' color='#007aff' />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Heading;
