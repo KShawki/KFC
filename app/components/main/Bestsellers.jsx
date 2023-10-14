a7e00eda58e11928ee3f941c644ff621651c59d7
Khalid Shawki <khalid.i.shawki@gmail.com>
2023-10-14 07:58:06 +0300

    Bestseller section

--- app/components/main/Bestsellers.jsx
+++ app/components/main/Bestsellers.jsx
@@ -1,4 +1,12 @@
 import Heading from './Heading';
+import './bestseller.css';
+
+const items = [
+  { img: '/bs/KanttokCombo.jpeg', title: 'Kantook Combo' },
+  { img: '/bs/ClassicBucket9Pcs.jpg', title: 'Classic Bucket (9 pcs)' },
+  { img: '/bs/8PcFestive.jpg', title: '8 Pc Festive Bucket' },
+  { img: '/bs/MightyZinger.jpeg', title: 'Mighty Zinger Sandwich' },
+];
 
 const Bestsellers = () => {
   return (
@@ -8,8 +16,17 @@ const Bestsellers = () => {
         img='/icons/best-sellers.png'
         viewAll='false'
       />
+      <div className='productList'>
+        {items.map((item, index) => (
+          <a href='#' key={index}>
+            <div>
+              <img src={item.img} alt={item.title} />
+              <div className='productListTitle'>{item.title}</div>
+            </div>
+          </a>
+        ))}
+      </div>
     </section>
   );
 };
-
 export default Bestsellers;
