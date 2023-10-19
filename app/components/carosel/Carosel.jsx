0c1df4e2ab55bca5b5515303836453fa235aaa26
Khalid Shawki <khalid.i.shawki@gmail.com>
2023-10-19 07:36:40 +0300

    Add Slider

--- app/components/carosel/Carosel.jsx
+++ app/components/carosel/Carosel.jsx
@@ -1,13 +1,35 @@
 'use client';
-import { useEffect } from 'react';
+import './carosel.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './carosel.css';
 
-import { Carousel } from 'react-responsive-carousel';
-// import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+const slides = [
+  { id: 1, text: 'Chililime', bgImage: '/slider/Chililime.jpeg' },
+  { id: 2, text: 'Duetto', bgImage: '/slider/Duetto.jpeg' },
+  { id: 3, text: 'Mega Rizo', bgImage: '/slider/MegaRizo.jpeg' },
+];
 
 const Slider = () => {
   return (
     <section id='slider' className='container'>
-      
+      <Swiper pagination={true} modules={[Pagination]} className='mySwiper'>
+        {slides.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div>
+              <img
+                src={item.bgImage}
+                width={1000}
+                height={200}
+                alt={item.text}
+                className='photo-slider'
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };
--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata = {
   title: 'KFC Official Site - Various Deals Available',
   description: 'Visit Our Website and Choose From Our Many Delicious Meals. Order Online Today. You Can Now Enjoy KFC With Our Safe and Contactless Deliveries. Delivery Around Egypt.',
+  icons: {
+    icon: '/icon.ico',
+  },
 }
 
 export default function RootLayout({ children }) {
